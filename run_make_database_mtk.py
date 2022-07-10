7707a781654571a84eeb3f6940a447249871963c
kaixu <kai.xu@cocopie.ai>
2022-07-09 19:04:43 -0700

    initial commit

--- run_make_database_mtk.py
+++ run_make_database_mtk.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 ##
 dir_data = './datasets/em'
+os.makedirs(dir_data, exist_ok=True)
 
 name_target = 'train-labels.tif'
 name_data = 'train-volume.tif'
@@ -15,75 +16,80 @@ img_data = Image.open(os.path.join(dir_data, name_data))
 ny, nx = img_target.size
 nframe = img_target.n_frames
 
-##
-nframe_train = 0   # The number of training set
-nframe_val = 0      # The number of validation set
-nframe_test = 3     # The number of test set
+def data_prepare(nframe_train, nframe_val, nframe_test):
+# The number of training set
+# The number of validation set
+# The number of test set
 
-# [30, 0, 0] -> [0, 30, 0] -> [0, 0, 3]
-dir_data = 'data_mtk/'
+    # [30, 0, 0] -> [0, 30, 0] -> [0, 0, 3]
+    dir_data = 'data_mtk/em'
 
-dir_save_train = os.path.join(dir_data, 'train')
-dir_save_val = os.path.join(dir_data, 'val')
-dir_save_test = os.path.join(dir_data, 'test')
+    dir_save_train = os.path.join(dir_data, 'train')
+    dir_save_val = os.path.join(dir_data, 'val')
+    dir_save_test = os.path.join(dir_data, 'test')
 
-if not os.path.exists(dir_save_train):
-    os.makedirs(dir_save_train)
+    if not os.path.exists(dir_save_train):
+        os.makedirs(dir_save_train)
 
-if not os.path.exists(dir_save_val):
-    os.makedirs(dir_save_val)
+    if not os.path.exists(dir_save_val):
+        os.makedirs(dir_save_val)
 
-if not os.path.exists(dir_save_test):
-    os.makedirs(dir_save_test)
+    if not os.path.exists(dir_save_test):
+        os.makedirs(dir_save_test)
 
-##
-id_frame = np.arange(nframe)
-np.random.shuffle(id_frame)
+    ##
+    id_frame = np.arange(nframe)
+    np.random.shuffle(id_frame)
 
-##
-offset_nframe = 0
+    ##
+    offset_nframe = 0
 
-for i in range(nframe_train):
-    img_target.seek(id_frame[i + offset_nframe])
-    img_data.seek(id_frame[i + offset_nframe])
+    for i in range(nframe_train):
+        img_target.seek(id_frame[i + offset_nframe])
+        img_data.seek(id_frame[i + offset_nframe])
 
-    target_ = np.asarray(img_target)
-    data_ = np.asarray(img_data)
+        target_ = np.asarray(img_target)
+        data_ = np.asarray(img_data)
 
-    np.save(os.path.join(dir_save_train, "input_%03d.npy" % i), (data_))
-    np.save(os.path.join(dir_save_train, "label_%03d.npy" % i), (target_))
+        np.save(os.path.join(dir_save_train, "input_%03d.npy" % i), (data_))
+        np.save(os.path.join(dir_save_train, "label_%03d.npy" % i), (target_))
 
-##
-offset_nframe = nframe_train
+    ##
+    offset_nframe = nframe_train
 
-for i in range(nframe_val):
-    img_target.seek(id_frame[i + offset_nframe])
-    img_data.seek(id_frame[i + offset_nframe])
+    for i in range(nframe_val):
+        img_target.seek(id_frame[i + offset_nframe])
+        img_data.seek(id_frame[i + offset_nframe])
 
-    target_ = np.asarray(img_target)
-    data_ = np.asarray(img_data)
+        target_ = np.asarray(img_target)
+        data_ = np.asarray(img_data)
 
-    np.save(os.path.join(dir_save_val, "input_%03d.npy" % i), (data_))
-    np.save(os.path.join(dir_save_val, "label_%03d.npy" % i), (target_))
+        np.save(os.path.join(dir_save_val, "input_%03d.npy" % i), (data_))
+        np.save(os.path.join(dir_save_val, "label_%03d.npy" % i), (target_))
 
-##
-offset_nframe = nframe_train + nframe_val
+    ##
+    offset_nframe = nframe_train + nframe_val
 
-for i in range(nframe_test):
-    img_target.seek(id_frame[i + offset_nframe])
-    img_data.seek(id_frame[i + offset_nframe])
+    for i in range(nframe_test):
+        img_target.seek(id_frame[i + offset_nframe])
+        img_data.seek(id_frame[i + offset_nframe])
 
-    target_ = np.asarray(img_target)
-    data_ = np.asarray(img_data)
+        target_ = np.asarray(img_target)
+        data_ = np.asarray(img_data)
 
-    np.save(os.path.join(dir_save_test, "input_%03d.npy" % i), (data_))
-    np.save(os.path.join(dir_save_test, "label_%03d.npy" % i), (target_))
+        np.save(os.path.join(dir_save_test, "input_%03d.npy" % i), (data_))
+        np.save(os.path.join(dir_save_test, "label_%03d.npy" % i), (target_))
 
-##
-plt.subplot(121)
-plt.imshow(target_)
+    ##
+    # plt.subplot(121)
+    # plt.imshow(target_)
+    #
+    # plt.subplot(122)
+    # plt.imshow(data_)
+    #
+    # plt.show()
 
-plt.subplot(122)
-plt.imshow(data_)
-
-plt.show()
+if __name__ == "__main__":
+    data_prepare(30, 0, 0)
+    data_prepare(0, 30, 0)
+    data_prepare(0, 0, 3)
